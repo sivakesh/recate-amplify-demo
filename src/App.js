fcabb2a30a9e72baf03c2089412be7bc510e1754
sivakesh <sivakesh@gmail.com>
2021-08-04 13:46:46 +0530

    Auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import awsConfig from './aws-exports';
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
+
+Amplify.configure(awsConfig);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Amplify</h1>
-      </header>
-    </div>
+    <AmplifyAuthenticator>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to Amplify</h1>
+          <AmplifySignOut />
+        </header>
+      </div>
+      </AmplifyAuthenticator>
   );
 }
 
